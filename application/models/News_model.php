cfa1203153d11b8fb6ff95ae5440572041a36b4f
Adam Fabian <afabian@crimsonagility.com>
2016-08-17 21:21:31 -0700

    Created news item routing and controllers.
    08/17/16

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -5,4 +5,14 @@ class News_model extends CI_Model{
     {
         $this->load->database();
     }
+
+    public function get_news($slug = FALSE){
+        if($slug === false){
+            $query = $this->db->get('news');
+            return $query->result_array();
+        }
+
+        $query = $this->db->get_where('news', array('slug' => $slug));
+        return $query->row_array();
+    }
 }
